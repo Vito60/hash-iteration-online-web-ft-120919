6db9e057e32a0233557d9e1eb27946742ae3da8f
Randall Williams <randallw1996@gmail.com>
2019-12-12 19:02:10 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,4 +14,7 @@ end
 def age_appropriate_birthday(birthdays)
   birthdays.each do |kids_name, age|
     if age < 12 
-      puts 
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
+end
